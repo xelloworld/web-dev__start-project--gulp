4e90eada6d95fc3c6651c79b18694f481d2080f3
Sergey <sergey@xelloworld.ru>
2021-08-27 11:02:47 +0500

    Добавил задачу для автоудаления файлов и автообновления браузера.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 const {src, dest, parallel, series, watch} = gulp;
 
 import gulp from 'gulp';
+import del from 'del';
 import fileInclude from 'gulp-file-include';
 import htmlMin from 'gulp-htmlmin';
+import browserSync from 'browser-sync';
+
+// Clean
+
+export const clean = () => {
+  return del('build');
+};
 
 // HTML
 
@@ -16,11 +24,34 @@ export const html = () => {
       collapseWhitespace: true
     }))
     .pipe(dest('build'))
+    .pipe(browserSync.stream())
+}
+
+// Browsersync
+
+export const server = () => {
+  browserSync.init({
+    server: {
+      baseDir: 'build'
+    },
+    ghostMode: {
+      clicks: false,
+      forms: false,
+      scroll: false
+    },
+    notify: false,
+    open: false,
+    online: true
+  });
+  watch('src/html/**/*.html', html);
 }
 
 // Default
+
 export default series(
+  clean,
   parallel(
     html
-  )
+  ),
+  server
 );
